20b285918e488343efa992fab2b88b3e1a90de41
KunalPandey36 <61598189+KunalPandey36@users.noreply.github.com>
2021-01-03 17:23:14 +0530

    Covac
    Vaccine distribution based application

---
+++ HackElite-2020-_Phoenix-master/lib/welcome.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'login.dart';
+import 'reg0.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Lottie.asset('lottie/covid.json'),
+                    height: 329.0,
+                    margin: EdgeInsets.all(20.0),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Login()),
+                    //Go to login screen.
+                  ),
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30.0),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Reg0()),
+                  ),
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register youself',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
